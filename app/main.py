bf23da3218d6aa6f6c96b7f8bedcfa1fbe36b41f
Valerii Kashpur <kashpur.v.f@gmail.com>
2024-11-29 17:39:04 +0200

    refactoring
    - human like code added

--- app/main.py
+++ app/main.py
@@ -4,23 +4,24 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people.update({name: self})
+        Person.people.update({name: self})
 
 
 def create_person_list(people: list) -> list:
-    list_of_persons_instances = [
+    persons_instances = [
         Person(person["name"],
                person["age"])
         for person in people
     ]
 
-    for i, person in enumerate(people):
+    for person in people:
+        instances_cache = Person.people
+        person_instance = instances_cache[person["name"]]
         get_wife = person.get("wife")
         get_husband = person.get("husband")
-        person_instance = list_of_persons_instances[i]
         if get_wife:
-            person_instance.wife = person_instance.people[get_wife]
+            person_instance.wife = instances_cache[get_wife]
         elif get_husband:
-            person_instance.husband = person_instance.people[get_husband]
+            person_instance.husband = instances_cache[get_husband]
 
-    return list_of_persons_instances
+    return persons_instances
