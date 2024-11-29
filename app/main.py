da3664721ce3dc7db9d460ad0969c2bcf0612a7d
Valerii Kashpur <kashpur.v.f@gmail.com>
2024-11-29 16:35:44 +0200

    cache peope
    - added people cache for Person class
    - added spouse reference for each people in cache

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people.update({name: self})
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    list_of_persons_instances = [
+        Person(person["name"],
+               person["age"])
+        for person in people
+    ]
+
+    for i, person in enumerate(people):
+        get_wife = person.get("wife")
+        get_husband = person.get("husband")
+        person_instance = list_of_persons_instances[i]
+        if get_wife:
+            person_instance.wife = person_instance.people[get_wife]
+        elif get_husband:
+            person_instance.husband = person_instance.people[get_husband]
+
+    return list_of_persons_instances
